8ed751029930bdd7a2ba42e5dc539e2503eff1a4
Piyush Keshri <piyushkeshri383@gmail.com>
2022-05-15 22:44:25 +0530

    created upload feature,progress bar and percentage uploaded

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const dropZone = document.querySelector(".drop-zone");
+const browseBtn = document.querySelector(".browse-btn");
+const fileInput = document.querySelector("#file-input");
+
+const bgProgress = document.querySelector(".bg-progress");
+const percentDiv = document.querySelector('#percent');
+
+const host = "https://innshare.herokuapp.com/";
+const uploadURL = `${host}api/files`;
+// const uploadURL = `${host}api/files`;
 
 dropZone.addEventListener("dragover", (e) => {
   e.preventDefault();
@@ -15,4 +24,39 @@ dropZone.addEventListener("dragleave", () => {
 dropZone.addEventListener("drop", (e) => {
   e.preventDefault();
   dropZone.classList.remove("dragged");
+  const files = e.dataTransfer.files;
+  if (files.length) {
+    fileInput.files = files;
+  }
 });
+
+fileInput.addEventListener("change", () => {
+  uploadFile();
+});
+
+browseBtn.addEventListener("click", () => {
+  fileInput.click();
+});
+
+const uploadFile = () => {
+  const file = fileInput.files[0];
+  const formData = new FormData();
+  formData.append("myfile", file);
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      console.log(xhr.response);
+    }
+  };
+
+  xhr.upload.onprogress = updateProgress;
+
+  xhr.open("POST", uploadURL);
+  xhr.send(formData);
+};
+
+const updateProgress = (e) =>{
+  const percent = Math.round((e.loaded / e.total) * 100);
+  bgProgress.style.width = `${percent}%`;
+  percentDiv.innerText = percent;
+} 
