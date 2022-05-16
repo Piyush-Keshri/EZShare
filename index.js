677c5d82da3e5f8e98c1c0bc64d8d3fd22cd59d6
Piyush Keshri <piyushkeshri383@gmail.com>
2022-05-16 12:21:17 +0530

    Added email box,submit button and created progress bar

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const bgProgress = document.querySelector(".bg-progress");
 const progressBar = document.querySelector(".progress-bar");
 const percentDiv = document.querySelector("#percent");
 
+const sharingContainer = document.querySelector(".sharing-container");
+const fileURLInput = document.querySelector("#fileURL");
+const copyBtn = document.querySelector("#copy-btn");
+
+const emailForm = document.querySelector("#email-form");
+
 const host = "https://innshare.herokuapp.com/";
 const uploadURL = `${host}api/files`;
-// const uploadURL = `${host}api/files`;
+const emailURL = `${host}api/files/send`;
 
 dropZone.addEventListener("dragover", (e) => {
   e.preventDefault();
@@ -40,6 +46,11 @@ browseBtn.addEventListener("click", () => {
   fileInput.click();
 });
 
+copyBtn.addEventListener("click", () => {
+  fileURLInput.select();
+  document.execCommand("copy");
+});
+
 const uploadFile = () => {
   progressContainer.style.display = "block";
   const file = fileInput.files[0];
@@ -49,7 +60,7 @@ const uploadFile = () => {
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       console.log(xhr.response);
-      showLink(JSON.parse(xhr.response));
+      onUploadSuccess(JSON.parse(xhr.response));
     }
   };
 
@@ -66,7 +77,35 @@ const updateProgress = (e) => {
   progressBar.style.transform = `scaleX(${percent / 100})`;
 };
 
-const showLink = ({ file }) => {
-  console.log(file);
+const onUploadSuccess = ({ file: url }) => {
+  console.log(url);
+  fileInput.value = "";
+  emailForm[2].removeAttribute("disabled");
   progressContainer.style.display = "none";
+  sharingContainer.style.display = "block";
+  fileURLInput.value = url;
 };
+
+emailForm.addEventListener("submit", () => {
+  e.preventDefault();
+  const url = fileURLInput.value;
+  const formData = {
+    uuid: "url.split(" / ").splice(-1,1)[0]",
+    emailTo: emailForm.elements["to-email"].value,
+    emailFrom: emailForm.elements["from-email"].value,
+  };
+  emailForm[2].setAttribute("disabled", "true");
+  fetch(emailURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((res) => res.json())
+    .then(({ success }) => {
+      if (success) {
+        sharingContainer.style.display = "none";
+      }
+    });
+});
